2c528290f5de04a2001a3071b07ffb77b4dea32f
Foodie <olaorefouad565@gmail.com>
2020-10-09 22:34:15 +0100

    first commit

---
+++ app/src/main/java/dev/olaore/unittestingonandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.olaore.unittestingonandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
